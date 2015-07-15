27d6df0358bbc0569f4bd31c800f5b4dd0cfc976
Jonathan Gines <jonathangines@hotmail.com>
2015-07-15 21:20:51 +0200

    corregido envio html

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
 var cool = require('cool-ascii-faces');
 var mongodb = require('mongodb');
 var assert = require('assert');
+var io = require('socket.io')(http);
 
 app.set('port', (process.env.PORT || 5000));
-app.engine('html', require('ejs').renderFile);
 
 app.get('/', function(request, response) {
-  response.render('pages/index.html')
+  response.sendfile('pages/index.html')
 });
 
 app.get('/cool', function(request, response) {
