13af84d4f581c1ebf29720ed82f7f498c179cf5e
Jonathan Gines <jonathangines@hotmail.com>
2015-07-15 11:05:50 +0200

    prueba index.html

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var mongodb = require('mongodb');
 var assert = require('assert');
 
 app.set('port', (process.env.PORT || 5000));
+app.use(express.static(path.join(__dirname, 'pages')));
 
 app.get('/', function(request, response) {
   response.render('pages/index.html')
