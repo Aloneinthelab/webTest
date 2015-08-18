fd7e3126114a574971c005aae67e452afb880e75
Jonathan Gines <jonathangines@hotmail.com>
2015-08-18 19:12:15 +0200

    incluida animacion de espera

--- public/js/profile.js
+++ public/js/profile.js
@@ -8,18 +8,47 @@ $(document).ready(function(){
     var socket = io.connect("http://aloneinthelab-web-test.herokuapp.com:8080");
   }
 	$("#startbutton").click(function(){
+    loadingGif();
+    $("#startbutton").push();
 		var username = $("#username").text();
 		var lang1 = $("#mainlanguage").text();
 		var lang2 = $("#seclanguage").text();
-  		console.log('Enviamos msg para entrar en la room: msg -- '+username+' -- '+lang1+' -- '+lang2);
-  		socket.emit('room',{ username: username ,
-  			lang1: lang1 ,
-  			lang2: lang2});
+  	console.log('Enviamos msg para entrar en la room: msg -- '+username+' -- '+lang1+' -- '+lang2);
+  	socket.emit('room',{ username: username ,
+  		lang1: lang1 ,
+  		lang2: lang2});
 
-  		socket.on('roomNumber',function (msg){
-    		console.log("Recibido numero de room: " + msg);
-    		var roomURL = '/room=' + msg + '&user=' + username + '&lang=a';
-   		  window.location = roomURL;
-    	});
-	});
+  	socket.on('roomNumber',function (msg){
+    	console.log("Recibido numero de room: " + msg);
+    	var roomURL = '/room=' + msg + '&user=' + username + '&lang=a';
+   	  window.location = roomURL;
+    });
+  });
 });
+
+function loadingGif() {
+  var circle = new Sonic({
+    width: 50,
+    height: 50,
+    padding: 50,
+ 
+    strokeColor: '#000',
+ 
+    pointDistance: .01,
+    stepsPerFrame: 3,
+    trailLength: .7,
+ 
+    step: 'fader',
+ 
+    setup: function() {
+        this._.lineWidth = 5;
+    },
+ 
+    path: [
+        ['arc', 25, 25, 25, 0, 360]
+    ]
+ 
+  });
+  circle.play();
+  document.body.appendChild(circle.canvas); 
+}
